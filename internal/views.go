5283103f8a001630efba6f459ab41b5e7e29dc39
Mizuba74 <kevinleal@hotmail.com>
2025-04-09 19:05:32 +0200

    add focus style login page

--- internal/views.go
+++ internal/views.go
@@ -1,11 +1,35 @@
 package main
 
-import "tui-apptrack/internal/misc"
+import (
+	"tui-apptrack/internal/misc"
+
+	"github.com/charmbracelet/lipgloss"
+)
+
+var (
+	focusedInputStyle = lipgloss.NewStyle().
+				Border(lipgloss.RoundedBorder()).
+				BorderForeground(lipgloss.Color("205"))
+
+	blurredInputStyle = lipgloss.NewStyle().
+				Border(lipgloss.NormalBorder()).
+				BorderForeground(lipgloss.Color("240"))
+)
 
 func (m Model) ViewLoginPage() string {
-	content := misc.Logo + "\n\nEnter your Email and password:\n\n" +
-		m.Login.EmailInput.View() + "\n" +
-		m.Login.PasswordInput.View() + "\n\nPress 'esc' to quit.\n"
+	emailStyle := blurredInputStyle
+	passwordStyle := blurredInputStyle
+
+	if m.Login.CurrentIndex == 0 {
+		emailStyle = focusedInputStyle
+	} else {
+		passwordStyle = focusedInputStyle
+	}
+
+	content := misc.Logo + "\n\n" +
+		emailStyle.Render(m.Login.EmailInput.View()) + "\n" +
+		passwordStyle.Render(m.Login.PasswordInput.View()) + "\n\n" +
+		"Press 'tab' to switch fields, 'enter' to submit, 'esc' to quit.\n"
 	return content
 }
 
